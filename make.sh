14b96833892a2a729b0dafe3c9c74def5a25d57e
Andres Villarroel <andres.villarroel@autodesk.com>
2019-01-29 16:11:39 -0800

    windows binaries

--- make.sh
+++ make.sh
@@ -4,6 +4,14 @@ set -e
 
 cd cmd/update-aws-ecs-service
 go get -v
+GOARCH=amd64 GOOS=windows go build -o update-aws-ecs-service-amd64-windows.exe
+zip update-aws-ecs-service-amd64-windows.zip update-aws-ecs-service-amd64-windows.exe
+rm update-aws-ecs-service-amd64-windows.exe
+mv update-aws-ecs-service-amd64-windows.zip ../../
+cd ../../
+
+cd cmd/update-aws-ecs-service
+go get -v
 GOARCH=amd64 GOOS=darwin go build -o update-aws-ecs-service-amd64-darwin
 zip update-aws-ecs-service-amd64-darwin.zip update-aws-ecs-service-amd64-darwin
 rm update-aws-ecs-service-amd64-darwin
@@ -16,6 +24,14 @@ GOARCH=amd64 GOOS=linux go build -o update-aws-ecs-service-amd64-linux
 zip update-aws-ecs-service-amd64-linux.zip update-aws-ecs-service-amd64-linux
 rm update-aws-ecs-service-amd64-linux
 cp update-aws-ecs-service-amd64-linux.zip ../../
+cd ../../
+
+cd cmd/enforce-aws-ecs-asg-launchconfig
+go get -v
+GOARCH=amd64 GOOS=windows go build -o enforce-aws-ecs-asg-launchconfig-amd64-windows.exe
+zip enforce-aws-ecs-asg-launchconfig-amd64-windows.zip enforce-aws-ecs-asg-launchconfig-amd64-windows.exe
+rm enforce-aws-ecs-asg-launchconfig-amd64-windows.exe
+mv enforce-aws-ecs-asg-launchconfig-amd64-windows.zip ../../
 cd ../../
 
 cd cmd/enforce-aws-ecs-asg-launchconfig
