0614bb2416f0d737fe670a8b89f5beb4e8a7088e
Andres Villarroel <andres.villarroel@autodesk.com>
2019-02-05 06:54:30 -0800

    better make.sh

--- make.sh
+++ make.sh
@@ -3,49 +3,51 @@
 set -e
 
 cd cmd/update-aws-ecs-service
-go get -v
-GOARCH=amd64 GOOS=windows go build -o update-aws-ecs-service-amd64-windows.exe
-zip update-aws-ecs-service-amd64-windows.zip update-aws-ecs-service-amd64-windows.exe
-rm update-aws-ecs-service-amd64-windows.exe
-mv update-aws-ecs-service-amd64-windows.zip ../../
+go get -f -u -v
+cd ../../
+
+cd cmd/enforce-aws-ecs-asg-launchconfig
+go get -f -u -v
 cd ../../
 
 cd cmd/update-aws-ecs-service
-go get -v
-GOARCH=amd64 GOOS=darwin go build -o update-aws-ecs-service-amd64-darwin
-zip update-aws-ecs-service-amd64-darwin.zip update-aws-ecs-service-amd64-darwin
-rm update-aws-ecs-service-amd64-darwin
-mv update-aws-ecs-service-amd64-darwin.zip ../../
+GOARCH=amd64 GOOS=windows go build -o update-aws-ecs-service.exe
+zip update-aws-ecs-service-amd64-windows.zip update-aws-ecs-service.exe
+rm update-aws-ecs-service.exe
+mv -f update-aws-ecs-service-amd64-windows.zip ../../
 cd ../../
 
 cd cmd/update-aws-ecs-service
-go get -v
-GOARCH=amd64 GOOS=linux go build -o update-aws-ecs-service-amd64-linux
-zip update-aws-ecs-service-amd64-linux.zip update-aws-ecs-service-amd64-linux
-rm update-aws-ecs-service-amd64-linux
-cp update-aws-ecs-service-amd64-linux.zip ../../
+GOARCH=amd64 GOOS=darwin go build -o update-aws-ecs-service
+zip update-aws-ecs-service-amd64-darwin.zip update-aws-ecs-service
+rm update-aws-ecs-service
+mv -f update-aws-ecs-service-amd64-darwin.zip ../../
+cd ../../
+
+cd cmd/update-aws-ecs-service
+GOARCH=amd64 GOOS=linux go build -o update-aws-ecs-service
+zip update-aws-ecs-service-amd64-linux.zip update-aws-ecs-service
+rm update-aws-ecs-service
+mv -f update-aws-ecs-service-amd64-linux.zip ../../
 cd ../../
 
 cd cmd/enforce-aws-ecs-asg-launchconfig
-go get -v
-GOARCH=amd64 GOOS=windows go build -o enforce-aws-ecs-asg-launchconfig-amd64-windows.exe
-zip enforce-aws-ecs-asg-launchconfig-amd64-windows.zip enforce-aws-ecs-asg-launchconfig-amd64-windows.exe
-rm enforce-aws-ecs-asg-launchconfig-amd64-windows.exe
-mv enforce-aws-ecs-asg-launchconfig-amd64-windows.zip ../../
+GOARCH=amd64 GOOS=windows go build -o enforce-aws-ecs-asg-launchconfig.exe
+zip enforce-aws-ecs-asg-launchconfig-amd64-windows.zip enforce-aws-ecs-asg-launchconfig.exe
+rm enforce-aws-ecs-asg-launchconfig.exe
+mv -f enforce-aws-ecs-asg-launchconfig-amd64-windows.zip ../../
 cd ../../
 
 cd cmd/enforce-aws-ecs-asg-launchconfig
-go get -v
-GOARCH=amd64 GOOS=darwin go build -o enforce-aws-ecs-asg-launchconfig-amd64-darwin
-zip enforce-aws-ecs-asg-launchconfig-amd64-darwin.zip enforce-aws-ecs-asg-launchconfig-amd64-darwin
-rm enforce-aws-ecs-asg-launchconfig-amd64-darwin
-mv enforce-aws-ecs-asg-launchconfig-amd64-darwin.zip ../../
+GOARCH=amd64 GOOS=darwin go build -o enforce-aws-ecs-asg-launchconfig
+zip enforce-aws-ecs-asg-launchconfig-amd64-darwin.zip enforce-aws-ecs-asg-launchconfig
+rm enforce-aws-ecs-asg-launchconfig
+mv -f enforce-aws-ecs-asg-launchconfig-amd64-darwin.zip ../../
 cd ../../
 
 cd cmd/enforce-aws-ecs-asg-launchconfig
-go get -v
-GOARCH=amd64 GOOS=linux go build -o enforce-aws-ecs-asg-launchconfig-amd64-linux
-zip enforce-aws-ecs-asg-launchconfig-amd64-linux.zip enforce-aws-ecs-asg-launchconfig-amd64-linux
-rm enforce-aws-ecs-asg-launchconfig-amd64-linux
-cp enforce-aws-ecs-asg-launchconfig-amd64-linux.zip ../../
+GOARCH=amd64 GOOS=linux go build -o enforce-aws-ecs-asg-launchconfig
+zip enforce-aws-ecs-asg-launchconfig-amd64-linux.zip enforce-aws-ecs-asg-launchconfig
+rm enforce-aws-ecs-asg-launchconfig
+mv -f enforce-aws-ecs-asg-launchconfig-amd64-linux.zip ../../
 cd ../../
