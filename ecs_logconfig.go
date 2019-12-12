654df52f28184b65f1e31a10b96c76b41f55fb68
andresvia <andres.via@gmail.com>
2019-12-12 12:51:48 -0800

    Add alterLogConfigurationLogDriverOptions

--- ecs_logconfig.go
+++ ecs_logconfig.go
@@ -40,3 +40,73 @@ func alterLogConfigurationLogDriverOptions(copy ecs.LogConfiguration, overrides 
 	}
 	return copy
 }
+
+
+func alterLogConfigurationLogDriverSecrets(copy ecs.LogConfiguration, overrides map[string]map[string]string) ecs.LogConfiguration {
+	optionChanged := false
+	knockOutDriver := ""
+	for logDriver, overrides := range overrides {
+		if copy.LogDriver != nil && *copy.LogDriver == logDriver {
+			for optionName, optionValue := range overrides {
+				if optionName == EnvKnockOutValue {
+					knockOutDriver = logDriver
+				}
+				optionChanged = true
+				thisOptionChanged := false
+				for _, secretOption := range copy.SecretOptions {
+					if secretOption != nil && secretOption.Name != nil && *secretOption.Name == optionName {
+						thisOptionChanged = true
+						secretOption.ValueFrom = aws.String(optionValue)
+					}
+				}
+				if ! thisOptionChanged {
+					copy.SecretOptions = append(copy.SecretOptions, &ecs.Secret{Name: aws.String(optionName), ValueFrom: aws.String(optionValue)})
+				}
+				var filteredSecretOptions []*ecs.Secret
+				for _, secretOption := range copy.SecretOptions {
+					if secretOption != nil && secretOption.Name != nil && *secretOption.Name == optionName {
+						if optionValue != EnvKnockOutValue && optionName != EnvKnockOutValue {
+							filteredSecretOptions = append(filteredSecretOptions, secretOption)
+						}
+					} else {
+						filteredSecretOptions = append(filteredSecretOptions, secretOption)
+					}
+				}
+				copy.SecretOptions = filteredSecretOptions
+			}
+		}
+	}
+	if knockOutDriver != "" {
+		optionChanged = false
+		delete(overrides, knockOutDriver)
+		copy.LogDriver = nil
+	}
+	if !optionChanged && len(overrides) == 1 {
+		for logDriver, options := range overrides {
+			copy.LogDriver = aws.String(logDriver)
+			for optionName, optionValue := range options {
+				thisOptionChanged := false
+				for _, secretOption := range copy.SecretOptions {
+					if secretOption != nil && secretOption.Name != nil && *secretOption.Name == optionName {
+						thisOptionChanged = true
+					}
+				}
+				if ! thisOptionChanged {
+					copy.SecretOptions = append(copy.SecretOptions, &ecs.Secret{Name: aws.String(optionName), ValueFrom: aws.String(optionValue)})
+				}
+				var filteredSecretOptions []*ecs.Secret
+				for _, secretOption := range copy.SecretOptions {
+					if secretOption != nil && secretOption.Name != nil && *secretOption.Name == optionName {
+						if optionValue != EnvKnockOutValue && optionName != EnvKnockOutValue {
+							filteredSecretOptions = append(filteredSecretOptions, secretOption)
+						}
+					} else {
+						filteredSecretOptions = append(filteredSecretOptions, secretOption)
+					}
+				}
+				copy.SecretOptions = filteredSecretOptions
+			}
+		}
+	}
+	return copy
+}
--- ecs_logconfig_test.go
+++ ecs_logconfig_test.go
@@ -17,23 +17,23 @@ func TestAlterLogConfigurationLogDriverOptions(t *testing.T) {
 		},
 		SecretOptions: []*ecs.Secret{},
 	}
-	alterLogConfigurationLogDriverOptions(logConfig, map[string]map[string]string{
+	logConfig = alterLogConfigurationLogDriverOptions(logConfig, map[string]map[string]string{
 		"mydriver": {
 			"myTouchedOption": "myTouchedOptionNewValue",
 			optionToDelete:    EnvKnockOutValue,
 		},
 	})
 	if *logConfig.LogDriver != "mydriver" {
-		t.Fail()
+		t.Fatal()
 	}
 	if *logConfig.Options["myUntouchedOption"] != "myUntouchedOptionOriginalValue" {
-		t.Fail()
+		t.Fatal()
 	}
 	if *logConfig.Options["myTouchedOption"] != "myTouchedOptionNewValue" {
-		t.Fail()
+		t.Fatal()
 	}
 	if len(logConfig.Options) != 2 {
-		t.Fail()
+		t.Fatal()
 	}
 }
 
@@ -49,7 +49,7 @@ func TestDeleteLogConfigurationLogDriverOptions(t *testing.T) {
 		},
 		SecretOptions: []*ecs.Secret{},
 	}
-	alterLogConfigurationLogDriverOptions(logConfig, map[string]map[string]string{
+	logConfig = alterLogConfigurationLogDriverOptions(logConfig, map[string]map[string]string{
 		"olddriver": {
 			"":              "ignored",
 			optionToDelete1: "",
@@ -59,13 +59,128 @@ func TestDeleteLogConfigurationLogDriverOptions(t *testing.T) {
 			optionToDelete2: "",
 		},
 	})
+	if *logConfig.LogDriver != "newdriver" {
+		t.Fatal()
+	}
 	if *logConfig.Options["myUntouchedOption"] != "myUntouchedOptionOriginalValue" {
-		t.Fail()
+		t.Fatal()
 	}
 	if *logConfig.Options["myOptionToAdd"] != "myOptionToAddValue" {
-		t.Fail()
+		t.Fatal()
 	}
 	if len(logConfig.Options) != 2 {
-		t.Fail()
+		t.Fatal()
+	}
+}
+
+func TestAlterLogConfigurationLogDriverSecrets(t *testing.T) {
+	optionToDelete := "myDeletedOption"
+	logConfig := ecs.LogConfiguration{
+		LogDriver: aws.String("mydriver"),
+		Options: map[string]*string{},
+		SecretOptions: []*ecs.Secret{
+			{
+				Name:      aws.String("myUntouchedSecretOption"),
+				ValueFrom: aws.String("myUntouchedSecretOptionOriginalValue"),
+			},
+			{
+				Name:      aws.String("myTouchedSecretOption"),
+				ValueFrom: aws.String("myTouchedSecretOptionOriginalValue"),
+			},
+			{
+				Name:      aws.String(optionToDelete),
+				ValueFrom: aws.String("myDeletedSecretOption"),
+			},
+		},
+	}
+	logConfig = alterLogConfigurationLogDriverSecrets(logConfig, map[string]map[string]string{
+		"mydriver": {
+			"myTouchedSecretOption": "myTouchedSecretOptionNewValue",
+			"myAddedSecretOption": "myAddedSecretOptionNewValue",
+			optionToDelete:    EnvKnockOutValue,
+		},
+	})
+	if *logConfig.LogDriver != "mydriver" {
+		t.Fatal()
+	}
+	for _, secretOption := range logConfig.SecretOptions {
+		if *secretOption.Name == "myUntouchedSecretOption" {
+			if *secretOption.ValueFrom != "myUntouchedSecretOptionOriginalValue" {
+				t.Fatal()
+			}
+		}
+		if *secretOption.Name == "myTouchedSecretOption" {
+			if *secretOption.ValueFrom != "myTouchedSecretOptionNewValue" {
+				t.Fatal()
+			}
+		}
+		if *secretOption.Name == optionToDelete {
+			t.Fatal()
+		}
+		if *secretOption.Name == "myAddedSecretOption" {
+			if *secretOption.ValueFrom != "myAddedSecretOptionNewValue" {
+				t.Fatal()
+			}
+		}
+	}
+	if len(logConfig.SecretOptions) != 3 {
+		t.Fatal()
+	}
+}
+
+func TestDeleteLogConfigurationLogDriverSecrets(t *testing.T) {
+	optionToDelete1 := "myDeletedOption1"
+	optionToDelete2 := "myDeletedOption2"
+	logConfig := ecs.LogConfiguration{
+		LogDriver: aws.String("myolddriver"),
+		Options: map[string]*string{},
+		SecretOptions: []*ecs.Secret{
+			{
+				Name:      aws.String("myUntouchedSecretOption"),
+				ValueFrom: aws.String("myUntouchedSecretOptionOriginalValue"),
+			},
+			{
+				Name:      aws.String(optionToDelete1),
+				ValueFrom: aws.String("myDeletedSecretOption1"),
+			},
+			{
+				Name:      aws.String(optionToDelete2),
+				ValueFrom: aws.String("myDeletedSecretOption2"),
+			},
+		},
+	}
+	logConfig = alterLogConfigurationLogDriverSecrets(logConfig, map[string]map[string]string{
+		"myolddriver": {
+			"": "ignore",
+			optionToDelete1:    EnvKnockOutValue,
+		},
+		"mynewdriver": {
+			"myAddedSecretOption": "myAddedSecretOptionNewValue",
+			optionToDelete2:    EnvKnockOutValue,
+		},
+	})
+	if *logConfig.LogDriver != "mynewdriver" {
+		t.Fatal()
+	}
+	for _, secretOption := range logConfig.SecretOptions {
+		if *secretOption.Name == "myUntouchedSecretOption" {
+			if *secretOption.ValueFrom != "myUntouchedSecretOptionOriginalValue" {
+				t.Fatal()
+			}
+		}
+		if *secretOption.Name == optionToDelete1 {
+			t.Fatal()
+		}
+		if *secretOption.Name == optionToDelete2 {
+			t.Fatal()
+		}
+		if *secretOption.Name == "myAddedSecretOption" {
+			if *secretOption.ValueFrom != "myAddedSecretOptionNewValue" {
+				t.Fatal()
+			}
+		}
+	}
+	if len(logConfig.SecretOptions) != 2 {
+		t.Fatal()
 	}
 }
